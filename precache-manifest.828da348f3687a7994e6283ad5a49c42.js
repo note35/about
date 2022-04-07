d8d7f8f359d7057f475eff54b6adb5664a455329
Kir Chou <note351@hotmail.com>
2022-04-07 11:59:35 +0900

    Updates

--- precache-manifest.828da348f3687a7994e6283ad5a49c42.js
+++ precache-manifest.828da348f3687a7994e6283ad5a49c42.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e85a9c1fd54b5ab67456f829e7415aa0",
+    "revision": "8ee7d8d8a4f601a62e3f9f2cbbaf566a",
     "url": "/about/index.html"
   },
   {
-    "revision": "bea47f181503fa40bc33",
+    "revision": "72679cffa9453900384a",
     "url": "/about/static/css/main.a7921a3b.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/about/static/js/2.b4933831.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "bea47f181503fa40bc33",
-    "url": "/about/static/js/main.d7d196d8.chunk.js"
+    "revision": "72679cffa9453900384a",
+    "url": "/about/static/js/main.81109610.chunk.js"
   },
   {
     "revision": "a0062db1e7ece8554a7a",
