1f8992000ad899bac79a0f92947afbd2f2ae65a0
Kir Chou <note351@hotmail.com>
2022-04-07 11:59:35 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/about/precache-manifest.388344b9b1cd49905adee700420e0e13.js"
+  "/about/precache-manifest.828da348f3687a7994e6283ad5a49c42.js"
 );
 
 self.addEventListener('message', (event) => {
