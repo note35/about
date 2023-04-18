3b4e56deb561b9bbf50c76a73b295e159e59ecaa
Kir Chou <note351@hotmail.com>
2023-04-18 22:39:41 +0900

    Add intern host slide and update the AboutItems and TalkItems

--- src/AboutItems.tsx
+++ src/AboutItems.tsx
@@ -30,13 +30,16 @@ export const AboutLinkItems = [
 export const AboutItems = [
   {
     "name": "Brief Bio",
-    "description": "Thanks for your interest! I’m Kir (a kind of cocktail🍸), a Software Engineer at [Google Japan](https://en.wikipedia.org/wiki/Google). I've worked for [A9 (Amazon Search)](https://en.wikipedia.org/wiki/A9.com), [IBM Security](https://en.wikipedia.org/wiki/IBM), and interned for [Marvell](https://en.wikipedia.org/wiki/Marvell_Technology_Group). Outside of work, I have been a part of the [PyCon Taiwan](https://tw.pycon.org/) organization since 2017 and focusing on both reviewing agenda and giving talks. I have a master’s degree in Computer Science from [NCTU](https://www.nctu.edu.tw/en)."
+    "description": "Thanks for your interest! I’m Kir (a kind of cocktail🍸), a Software Engineer at [Google Japan](https://en.wikipedia.org/wiki/Google). I've worked for [A9 (Amazon Search)](https://en.wikipedia.org/wiki/A9.com), [IBM Security](https://en.wikipedia.org/wiki/IBM), and interned for [Marvell](https://en.wikipedia.org/wiki/Marvell_Technology_Group). Outside of work, I have been a part of the [PyCon Taiwan](https://tw.pycon.org/) organization since 2017 and focusing on both reviewing agenda and giving talks. I have a master’s degree in Computer Science from [NCTU](https://www.nctu.edu.tw/) (merged to NYCU)."
   }, {
     "name": "Career Focuses",
-    "description": "Python, C++, Build System, and Software Development Life Cycle (SDLC)."
+    "description": "Python, JavaScript, C++, Build System, and Software Development Life Cycle, Web Application."
   }, {
     "name": "Offline Studies",
-    "description": "absl::flat_hash_map, History of Python dict and hash, and Python Virtual Machine."
+    "description": "Paxos, a little bit Web3."
+  }, {
+    "name": "Past Studies",
+    "description": "PEG, absl::flat_hash_map, History of Python dict and hash, and Python Virtual Machine."
   }, {
     "name": "Recommended Tech Podcasts",
     "description": "[PyCast (mandarin only)](https://pycast.firstory.io/), [Python Bytes](https://pythonbytes.fm/), [The Python Podcast.__init__](https://www.pythonpodcast.com/), [CppCast](https://cppcast.com/), [Acquired](https://www.acquired.fm/), and [LadyBug Podcast](https://www.ladybug.dev/)."
