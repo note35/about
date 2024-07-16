1608134d8d02970978d23058d5bdd61aa80ecb7f
Kir Chou <note351@hotmail.com>
2024-07-17 07:38:18 +0900

    Update dependencies.

--- src/Utils.tsx
+++ src/Utils.tsx
@@ -74,7 +74,7 @@ export function MD2Html(text: string) {
   return (
     convert(text).map(item => {
       if(isUrlType(item)) {
-        return <Link target="_blank" href={item.url}>{item.text}</Link>
+        return <Link key={item.text} target="_blank" href={item.url}>{item.text}</Link>
       } else {
         return item
       }
